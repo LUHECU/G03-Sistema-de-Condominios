e4f41e83e45d4fcd98a838974e35263fa6335811
Luis Hernandez <luis.m.hernandez.cruz@gmail.com>
2024-09-15 09:31:58 -0600

    Class1

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace G03_Sistema_de_Condominios
+{
+    public class Class1
+    {
+
+       //....
+
+
+    }
+}
